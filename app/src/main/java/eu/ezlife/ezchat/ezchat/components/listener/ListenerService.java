63d725a0ba2262dcce4c101875ce35e8aa596201
Philipp Minges <fjnalta@gmail.com>
2017-04-11 20:38:54 +0200

    add new listener service

---
+++ app/src/main/java/eu/ezlife/ezchat/ezchat/components/listener/ListenerService.java
@@ -0,0 +1,8 @@
+package eu.ezlife.ezchat.ezchat.components;
+
+/**
+ * Created by ajo on 11.04.17.
+ */
+
+public interface MessageHandler {
+}
---
+++ app/src/main/java/eu/ezlife/ezchat/ezchat/components/listener/MessageHandler.java
@@ -0,0 +1,8 @@
+package eu.ezlife.ezchat.ezchat.components.listener;
+
+/**
+ * Created by ajo on 11.04.17.
+ */
+
+public class MessageHandler {
+}
