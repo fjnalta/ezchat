4bb20cbb8eef88a58007de0fdd7151b6aeae396c
Philipp Minges <fjnalta@gmail.com>
2017-04-23 01:38:31 +0200

    change todo

--- app/src/main/java/eu/ezlife/ezchat/ezchat/components/firebaseServices/MyFireBaseMessageService.java
+++ app/src/main/java/eu/ezlife/ezchat/ezchat/components/firebaseServices/MyFireBaseMessageService.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -23,32 +22,27 @@ public class MyFireBaseMessageService extends FirebaseMessagingService {
 
     /**
      * Called when message is received.
-     *
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
-        Log.d("FirebaseCloudMessaging", "PUSH NOTIFICATION CALLED");
         super.onMessageReceived(remoteMessage);
-        // TODO - read contact name from database
         createNotification(remoteMessage.getData().get("contact"));
     }
 
     /**
      * Create and show a simple notification containing the received FCM message.
-     *
      * @param messageBody FCM message body received.
      */
     private void createNotification(String messageBody) {
         Intent intent = new Intent(this, ContactListActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_chat_send)
+                // TODO - get contact Name from Database
                 .setContentText("New Message from: " + messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
@@ -57,6 +51,6 @@ public class MyFireBaseMessageService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
