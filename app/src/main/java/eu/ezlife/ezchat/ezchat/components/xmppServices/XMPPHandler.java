fcfde1bac4f3ba9abd338e095b625661bd3726bd
Philipp Minges <fjnalta@gmail.com>
2017-04-26 18:27:41 +0200

    asdfdfgdfgdfg

---
+++ app/src/main/java/eu/ezlife/ezchat/ezchat/components/xmppServices/XMPPHandler.java
@@ -0,0 +1,8 @@
+package eu.ezlife.ezchat.ezchat.components.xmppServices;
+
+/**
+ * Created by ajo on 26.04.17.
+ */
+
+public class XMPPHandler {
+}
---
+++ app/src/main/java/eu/ezlife/ezchat/ezchat/data/ObserverObject.java
@@ -0,0 +1,8 @@
+package eu.ezlife.ezchat.ezchat.data;
+
+/**
+ * Created by ajo on 26.04.17.
+ */
+
+public class ObserverObject {
+}
